1d8df0fb7ed2d25e5593f2be98a7d2b9068d5f9a
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2026-04-23 08:24:06 -0400

    made exit garage work

--- garage.py
+++ garage.py
@@ -10,7 +10,8 @@ def enter_garage(garage, car_id, entry_hour):
     return True
 
 def exit_garage(garage, car_id):
-    pass
+    del garage["cars"][car_id]
+    return True
 
 def get_available_spots(garage): 
     pass
