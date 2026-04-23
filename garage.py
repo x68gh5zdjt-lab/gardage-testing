4aba3ea56397a8b7f223ad92e54c1d74eacc94be
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2026-04-23 08:19:29 -0400

    Fixxed error

--- garage.py
+++ garage.py
@@ -1,6 +1,8 @@
 def enter_garage(garage, car_id, entry_hour):
     if len(garage["cars"]) >= garage["capacity"]:
         raise ValueError
+    if car_id in garage["cars"]:
+        raise ValueError
     garage["cars"][car_id] = entry_hour
     return True
 
